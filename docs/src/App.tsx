7bfaf95ff884dcf92087721e94a88bf5e6d06a9e
wangxingkang <wang_xingkang@qq.com>
2023-07-12 00:50:00 +0800

    docs: 示例优化

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,9 +1,25 @@
-import { Button } from '@sensoro-design/react';
+import { Button, ImageComparer } from '@sensoro-design/react';
 
 export const App = () => {
   return (
     <>
       <Button>Primary</Button>
+
+      <br />
+      <br />
+
+      <ImageComparer position={30}>
+        <img
+          slot="before"
+          src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80&sat=-100&bri=-5"
+          alt="Grayscale version of kittens in a basket looking around."
+        />
+        <img
+          slot="after"
+          src="https://images.unsplash.com/photo-1517331156700-3c241d2b4d83?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80"
+          alt="Color version of kittens in a basket looking around."
+        />
+      </ImageComparer>
     </>
   )
 }
