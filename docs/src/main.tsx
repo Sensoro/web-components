d02e626152d6e1cda6368f9657df85b5c4a843d9
wangxingkang <wang_xingkang@qq.com>
2023-07-12 10:33:44 +0800

    chore(docs): add @shoelace-style/shoelace

--- docs/src/main.tsx
+++ docs/src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { App } from './App.tsx';
 import '@sensoro-design/web-components/es/themes/default.css';
+import '@shoelace-style/shoelace/dist/themes/light.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />,
