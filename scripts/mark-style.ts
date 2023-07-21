7a8b8376c4f53e7a6634681f21c2bd6c4bcca331
wangxingkang <wang_xingkang@qq.com>
2023-07-21 10:10:46 +0800

    chore: react 包添加主题文件

--- scripts/mark-style.ts
+++ scripts/mark-style.ts
@@ -5,16 +5,14 @@ import { glob, fsExtra } from '@umijs/utils';
 import { watch } from './.internal/watch';
 import { PATHS } from './.internal/constants.js';
 
-const commandLineArgs = require('command-line-args')
+const commandLineArgs = require('command-line-args');
 
 const args = commandLineArgs([{ name: 'watch', type: Boolean }]);
 
 async function componentStyle() {
   const files = glob.sync('src/components/**/*.less', {
     cwd: PATHS.WEB_COMPONENTS,
-    ignore: [
-      '**/internal/**',
-    ],
+    ignore: ['**/internal/**'],
   });
 
   for (const file of files) {
@@ -35,7 +33,7 @@ async function componentStyle() {
       `,
       {
         parser: 'typescript',
-      }
+      },
     );
 
     /** 编辑 less */
@@ -46,16 +44,22 @@ async function componentStyle() {
 async function theme() {
   const files = glob.sync('src/themes/*.less', {
     cwd: PATHS.WEB_COMPONENTS,
-    ignore: [
-      '**/internal/**',
-    ],
+    ignore: ['**/internal/**'],
   });
 
   for (const file of files) {
+    console.log(file);
+    const fileName = file.split('/')[file.split('/').length - 1];
     const lessPath = path.join(PATHS.WEB_COMPONENTS, file);
+    const cssPath = lessPath.replace('.less', '.css');
+
+    const reactThemes = path.join(PATHS.REACT, 'themes');
 
     /** 编辑 less */
-    await $`lessc ${lessPath} ${lessPath.replace('.less', '.css')} --js`;
+    await $`lessc ${lessPath} ${cssPath} --js`;
+
+    fsExtra.mkdirSync(reactThemes, { recursive: true });
+    fsExtra.copyFileSync(cssPath, path.join(reactThemes, fileName.replace('.less', '.css')));
   }
 }
 
@@ -66,8 +70,8 @@ async function theme() {
 
 if (args.watch) {
   watch(['src/components/**/*.less'], {
-    cwd: PATHS.WEB_COMPONENTS
+    cwd: PATHS.WEB_COMPONENTS,
   }).on('change', async () => {
     await componentStyle();
-  })
+  });
 }
