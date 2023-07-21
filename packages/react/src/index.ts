b257f7d5bea53cb13a163b5d806597a531bd0d1e
wangxingkang <wang_xingkang@qq.com>
2023-07-21 10:27:44 +0800

    fix(icon): 解决样式不生效问题

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
 export { default as Button } from './components/Button/index.js';
 export { default as Icon } from './components/Icon/index.js';
-export { default as Switch } from './components/Switch/index.js';
 export { default as ImageComparer } from './components/ImageComparer/index.js';
+export { default as Switch } from './components/Switch/index.js';
